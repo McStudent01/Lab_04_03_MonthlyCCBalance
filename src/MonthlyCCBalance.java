a84bf206c59a3f3c720fa72b9f78e38ede7b6863
McStudent01 <mcmillcy@mail.uc.edu>
2023-05-31 10:29:01 -0500

    final commit

--- src/MonthlyCCBalance.java
+++ src/MonthlyCCBalance.java
@@ -2,6 +2,11 @@ public class MonthlyCCBalance
 {
     public static void main(String[] args)
     {
-        
+        double creditCardBalance = 5000;
+        double TAX_RATE = .17;
+        double oneMonth = creditCardBalance * TAX_RATE;
+        double twoMonth = oneMonth * TAX_RATE;
+        System.out.println("With a starting credit card balance of $5000, if no payments were made to this account after one month, the balance would be " +oneMonth + ".");
+        System.out.println("The balance if no payment is made after two months would be " + twoMonth + ".");
     }
 }
